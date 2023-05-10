636e4d993af1ba3903f68cb04ecd44e49bf58e07
bryanskinner <bryanskinner99@gmail.com>
2023-05-09 19:32:56 -0500

    finished the testing for ramp numbers

--- main.js
+++ main.js
@@ -16,27 +16,6 @@ const rl = readline.createInterface({
 
   // Your code here
 
-
-
-  function handleSubmit(event) {
-    event.preventDefault(); // prevent the default form submission behavior
-  
-    // get the form data
-    const form = event.target.form;
-    const formData = new FormData(form);
-  
-    // do something with the form data
-    // e.g. send it to the server using fetch or XMLHttpRequest
-  
-    console.log(formData); // log the form data to the console for debugging
-  }
-  
-  // attach the handleSubmit function to the submit button's onclick event
-  const submitButton = document.getElementById('submit');
-  submitButton.onclick = submit;
-
-  
-
 //* Checks if the inputed number is a ramp number, and if not returning false
   function isRampNumber(number) {
     const integer = number.toString().split('');
--- test.js
+++ test.js
@@ -1,50 +1,85 @@
-function isRampNumber() {
-    // Get the user input from the input field
-    const userInput = parseInt(document.querySelector('input[type="number"]').value);
+'use strict';
+
+
+// Your code here
+
+function handleSubmit(event) {
+  event.preventDefault(); 
   
-    // Check if the input is a positive integer
-    if (isNaN(userInput) || userInput <= 0 || !Number.isInteger(userInput)) {
-      // Display an error message if the input is not valid
-      const yesNoDiv = document.querySelector('.yes_no');
-      yesNoDiv.textContent = 'Please enter a positive integer.';
-      yesNoDiv.classList.remove('yes');
-      yesNoDiv.classList.remove('no');
-      return;
+  const form = event.target.form;
+  const number = form.number.value;
+
+  const result = numOfRampsBelow(Number(number));
+  const output = document.getElementById('output');
+  output.innerText = result;
+}
+
+// Checks if the inputted number is a ramp number, and if not returning false
+function isRampNumber(number) {
+  const integer = number.toString().split('');
+  for (let i = 1; i < integer.length; i++) {
+    if (integer[i] < integer[i - 1]) {
+      return false;
     }
+  }
+  return true;
+}
+
+// Counts the number of ramp numbers below the inputted number
+const numOfRampsBelow = (number) => {
+  const integer = number.toString().split('');
+  for (let i = 1; i < integer.length; i++) {
+    if (integer[i] < integer[i - 1]) {
+      return `This is not a ramp number`;
+    }
+  }
+
+  let count = 0;
+
+  for (let i = 0; i < number; i++) {
+    if (isRampNumber(i)) {
+      count++;
+    }
+  }
+  const message = `This is a Ramp Number. There are ${count} total ramp numbers less than ${number}`;
+  return message;
+}
+
+
+
+const getPrompt = () => {
+    rl.question('whatNumber: ', (answer) => {
+      console.log(numOfRampsBelow(answer));
+      getPrompt();
+    });
+  }
   
-    // Count the number of ramp numbers up to the user input
-    let count = 0;
-    const rampNumbers = [];
-    for (let i = 1; i <= userInput; i++) {
-      const digits = i.toString().split('');
-      let isRamp = true;
-      for (let j = 0; j < digits.length - 1; j++) {
-        if (parseInt(digits[j]) > parseInt(digits[j+1])) {
-          isRamp = false;
-          break;
-        }
-      }
-      if (isRamp) {
-        count++;
-        rampNumbers.push(i);
-      }
-    }
+  // Unit Tests
+  // to use them run the command: npm test main.js
+  // to close them ctrl + C
+  if (typeof describe === 'function') {
+    describe('#isRampNumber', () => {
+      it('should return false for non-ramp numbers', () => {
+        equal(isRampNumber(1032), false);
+        equal(isRampNumber(1528), false);
+        equal(isRampNumber(738), false);
+      });
   
-    // Update the result container with the count and list of ramp numbers
-    const yesNoDiv = document.querySelector('.yes_no');
-    const resultCountDiv = document.querySelector('.result_count');
-    const listContainer = document.querySelector('.list_container ul');
-    if (count === 0) {
-      yesNoDiv.textContent = 'NO';
-      yesNoDiv.classList.remove('yes');
-      yesNoDiv.classList.add('no');
-      resultCountDiv.textContent = `There are no ramp numbers up to ${userInput}.`;
-      listContainer.innerHTML = '';
-    } else {
-      yesNoDiv.textContent = 'YES';
-      yesNoDiv.classList.remove('no');
-      yesNoDiv.classList.add('yes');
-      resultCountDiv.textContent = `There are ${count} ramp numbers up to ${userInput}:`;
-      listContainer.innerHTML = rampNumbers.map(num => `<li>${num}</li>`).join('');
-    }
+      it('should return true for ramp numbers', () => {
+        equal(isRampNumber(1124), true);
+        equal(isRampNumber(13569), true);
+        equal(isRampNumber(0), true);
+        equal(isRampNumber(11111), true);
+      });
+  
+      it('should return the number of ramp numbers less than the given number, and a message string', () => {
+        equal(numOfRampsBelow(123), 'This is a Ramp Number. There are 65 total ramp numbers less than 123');
+        equal(numOfRampsBelow(456), 'This is a Ramp Number. There are 171 total ramp numbers less than 456');
+        equal(numOfRampsBelow(4578), 'This is a Ramp Number. There are 620 total ramp numbers less than 4578');
+      });
+    });
+  } else {
+    // attach the handleSubmit function to the submit button's onclick event
+    const submitButton = document.getElementById('submit');
+    submitButton.onclick = handleSubmit;
   }
